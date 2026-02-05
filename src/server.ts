04ae261650a69322bcb1fa71812e413450843a78
gaulthier-mre-01 <g.meurie@students.ephec.be>
2026-02-05 15:21:43 +0100

    exo 6.1

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const etudiants = [
+  { id: 1, nom: "Dupont", prenom: "Jean" },
+  { id: 2, nom: "Martin", prenom: "Sophie" },
+  { id: 3, nom: "Doe", prenom: "John" },
+];
+
 app.get('/', (req, res) => {
   res.send('Bienvenue sur mon serveur API');
+});
+
+app.get('/api/data', (req, res) => {
+  res.json(etudiants);
 });
 
 app.listen(port, () => {
